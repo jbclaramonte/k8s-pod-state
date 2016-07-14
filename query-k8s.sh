81597f0e2ab19f15b910d52e3a7a5ad0a53963f3
Jean-Baptiste Claramonte <jeanbaptiste.claramonte@gmail.com>
2016-07-14 13:17:34 +0200

    Update query-k8s.sh

--- query-k8s.sh
+++ query-k8s.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for 1 in {1..60} do
+for (( i=1; i<=30; c++ ))
+do
   curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://kubernetes/
   sleep 5
 done
